3395161adaf8c3c6e192c9d6b3c85c6f9d45de9d
maenmalkawi <150237429+maenmalkawi@users.noreply.github.com>
2023-12-04 17:36:13 +0300

    new

--- js/home.js
+++ js/home.js
@@ -52,23 +52,51 @@
 // var nav4 = null;
 // console.log("this var is ",typeof(nav4),":",nav4);
 
+//alert('hello');
 
-var h1 = document.getElementById('HJS');
-//  textContact or innerHTML contain the text inside the tag
+// var nav='hi iam js'; //declar var type is : string
+// console.log('hi iam maen');
+//console.log(nav);
+ //console.log(typeof(nav)); 
+//الtype of بعطيني نوع السطر يلي كتبته
+//console.log("this var is",typeof(nav),":",nav); //print the var type and value
+//nav=25; //ressign the value of var 
+//console.log("this var is",typeof(nav),":",nav);
+//console.log('#'.repeat(20))
+// مره 20 # ال علامه  بتطبع الريبيت
+
+//const nav2='hi iam nav2';
+//nav2=23; //constيعني ثابت ما بنقدر نغير قيمته
+// console.log("this const is ",typeof(nav2),":",nav2);
+
+// var nav3;
+// console.log("this const is ",typeof(nav3),":",nav3); //undefined nav3غير معرف لانه ما مال الها قيمه 
+// nav3=25;
+// console.log("this const is ",typeof(nav3),":",nav3);
+
+//var nav4=null;
+//console.log("this const is ",typeof(nav4),":",nav4);
+
+var h1= document.getElementById('maen');
+//  textContact or innerHTML contain the text inside the tag بتجيب التكست عن طريق التيكست او عن طريق ال اينر
+//console.log(h1);
 console.log(h1.innerHTML);
 //  change the text inside the tag
-h1.innerHTML = 'JS is awesome';
-//  change the style of the tag
-h1.style.color = 'red';
-h1.style.backgroundColor = 'black';
-//  
+//الاينر بتجيب حرفي لو كان بتجيب ال بوتن او الدايف كامل وهيك
+// التيكست بتجيب شو دوا البوتن او جوا ال الدايف
+h1.innerHTML='malkawii';//h1 الجمله يلي بتعرل ال 
 
-var img = document.getElementsByClassName('contantImg');
+h1.style.color='black'; //change the style color for h1
+h1.style.backgroundColor='white';//change the stylefor backgroundcolor for h1
+
+var img = document.getElementsByTagName('img');
 console.log(img);
-img[0].src='https://www.w3schools.com/js/pic_bulbon.gif';
-img[4].src='https://www.w3schools.com/js/pic_bulbon.gif';
-img[2].src='https://www.w3schools.com/js/pic_bulbon.gif';
-img[1].width= '100';
+img[1].src='https://www.w3schools.com/js/pic_bulbon.gif';
+img[3].width= '1';
+
+img[7].src='https://www.w3schools.com/js/pic_bulbon.gif';
+
+
 
 //   array 
 
